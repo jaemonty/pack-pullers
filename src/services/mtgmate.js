c47491b5f65ac7a4927e1d986983db12183bbc66
jaemonty <jaemonty@hotmail.com>
2026-08-09 13:27:41 +0930

    Refine Pack Pullers arcade homepage
    * Refine Pack Pullers arcade homepage
    
    * Refine Pack Pullers arcade homepage
    
    * Refine Pack Pullers arcade homepage
    
    * Add exact MTG Mate card URL support
    
    * Add exact MTG Mate card URL support

---
+++ src/services/mtgmate.js
@@ -0,0 +1,17 @@
+/** @typedef {{name:string,set:string,collectorNumber:string,finish:"foil"|"nonfoil"}} MtgMateCardIdentity */
+
+/** @param {string} name */
+export function slugifyMtgMateCardName(name) {
+  return name.normalize("NFKC").trim().replace(/\s*\/\/\s*/g, "_//_").replace(/\s+/g, "_");
+}
+
+/** @param {MtgMateCardIdentity["finish"]} finish */
+export function getMtgMateFinishSuffix(finish) {
+  return finish === "foil" ? ":foil" : "";
+}
+
+/** @param {MtgMateCardIdentity} card */
+export function buildMtgMateCardUrl(card) {
+  return `https://www.mtgmate.com.au/cards/${slugifyMtgMateCardName(card.name)}/${encodeURIComponent(card.set.toUpperCase())}/${encodeURIComponent(card.collectorNumber)}${getMtgMateFinishSuffix(card.finish)}`;
+}
+
